713ad387021e5cb5108c22158205557a7cd25944
Enrique Galiana <enrikegalianafdezcalvillo@gmail.com>
2025-07-14 01:03:56 +0200

    Initial commit test 1

--- src/main/java/net/gali/apm/event/ModEvents.java
+++ src/main/java/net/gali/apm/event/ModEvents.java
@@ -18,12 +18,22 @@ public class ModEvents {
 
     @SubscribeEvent
     public static void addCustomTrades(VillagerTradesEvent event){
+
+        // este if es para asegurarnos de que solo se añadan los trades al Badulaque
         if(event.getType() == ModVillagers.BADULAQUE.get()){
+
+            // ignora esto es pa que pille el trades del aldeano
             Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
 
+            // el trades.get(1) es el nivel del aldeano, en este caso nivel 1, tiene hastaa 5 niveles
             trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
+                    // el primer ItemStack es el que el aldeano quiere, en este caso 16 monedas APM
                     new ItemStack(ModItems.APM_COIN.get(), 16),
+                    // el segundo ItemStack es el que el aldeano te da, en este caso una Eneryeti de Sandía
                     new ItemStack(ModItems.WATERMELON_ENERYETI.get(), 1), 6, 10, 0.02f));
         }
+
+        // cuando termines de añadir los trades acuerdate que el aldeano se tiene que
+        // actualizar, y que usa un eneryetizador para el trabajo de badulaque
     }
 }
